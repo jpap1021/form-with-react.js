0efceb919875286caa85e3553ec9b5c82dbb315e
jpap1021 <juan.arteaga8710@gmail.com>
2019-03-18 11:52:11 -0500

    inputs

--- src/App.js
+++ src/App.js
@@ -18,7 +18,19 @@ constructor(){
     return (
      <main>
        <form>
-         
+        <input 
+              name="firstName" 
+              value={this.state.firstName} 
+              placeholder="First name" /><br/> 
+        <input 
+              name="lastName" 
+              value={this.state.lastname} 
+              placeholder="Last name" /><br/> 
+        <input 
+              name="age" 
+              value={this.state.age} 
+              placeholder="Age" /><br/> 
+        
        </form>
      </main>
     );
